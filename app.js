4c1a21d46286ea4a4d73b1ab8eb5bcbe8e35a7e2
Ivann <ivann.galic@edu.itescia.fr>
2020-02-06 14:59:01 +0100

    color bouton vert ok

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var themeChoice = [];
 var turn = 0;
 var userChoice;
 var correct;
-
+var entry;
 
 var questionsInsolite = [];
 questionsInsolite.push(["Quel est l'âge d'Arielle Dombasle ?", "113 ans", "Personne ne le sait", "66 ans", "La réponse D", "C"]);
@@ -49,6 +49,7 @@ function answer(themeChoice) {
 
   if (userChoice === correct) {
     document.getElementById("question").innerHTML = "Correct !";
+    document.getElementById("img-principale").innerHTML = '<img src="https://media1.tenor.com/images/c999da20a6b3f9278cfc059c4313ed32/tenor.gif?itemid=14294403" alt="gif yes">';
   } else {
     document.getElementById("question").innerHTML = "Perdu !";
 
@@ -57,18 +58,24 @@ function answer(themeChoice) {
         document.getElementById("A").className = "green";
         break;
       case "B" :
-        document.getElementById("B").repbtn = "green";
+        document.getElementById("B").className = "green";
         break;
       case "C" :
-        document.getElementById("C").repbtn = "green";
+        document.getElementById("C").className = "green";
         break;
       case "D" :
-        document.getElementById("D").repbtn = "green";
+        document.getElementById("D").className = "green";
         break;
     }
   }
 
   turn++;
+}
+
+function displayName() {
+  entry = document.getElementById('data').value;
+  document.getElementById("name").innerHTML=entry;
+  console.log(entry);
 }
 
 function insolite() {
