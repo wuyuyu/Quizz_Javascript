6fd872e362e8a4209608fd835463c123ffb544f1
Ivann <ivann.galic@edu.itescia.fr>
2020-02-06 10:53:08 +0100

    timer

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+var myTimer;
+var timeLeft;
+
+var questionsInsolite = [];
+questionsInsolite.push("Quel est l'âge d'Arielle Dombasle ?", "113 ans", "Personne ne le sait", "66 ans", "La réponse D", 3);
+questionsInsolite.push("test", "113 ans", "Personne ne le sait", "66 ans", "La réponse D", 3);
+console.log(questionsInsolite);
+
+function timer() {
+  timeLeft = 15;
+  clearInterval(myTimer);
+  myTimer = setInterval(function(){
+    document.getElementById("time-btn").innerHTML = "<i class=\"fas fa-hourglass-half\"></i> " + timeLeft;
+    timeLeft -= 1;
+
+    if(timeLeft < 0 ){
+      clearInterval(myTimer);
+      document.getElementById("time-btn").innerHTML = "Fini !";
+    }
+  }, 1000);
+}
+
 function joker() {
   document.getElementById("joker-btn").innerHTML='<i class="fas fa-phone-slash"></i>';
 }
