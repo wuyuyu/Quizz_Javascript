f64ee45d2218fe7d80f93ee301621c1eb00a6cd0
Ivann <ivann.galic@edu.itescia.fr>
2020-02-06 11:54:24 +0100

    affiche question/réponse + thèmes

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 var myTimer;
 var timeLeft;
+var themeChoice = [];
+var turn = 0;
+
 
 var questionsInsolite = [];
-questionsInsolite.push("Quel est l'âge d'Arielle Dombasle ?", "113 ans", "Personne ne le sait", "66 ans", "La réponse D", 3);
-questionsInsolite.push("test", "113 ans", "Personne ne le sait", "66 ans", "La réponse D", 3);
+questionsInsolite.push(["Quel est l'âge d'Arielle Dombasle ?", "113 ans", "Personne ne le sait", "66 ans", "La réponse D", "C"]);
+questionsInsolite.push(["question", "rep1", "test", "rep3", "test", "B"]);
+
+var questionsCultureG = [];
+questionsCultureG.push(["question culture ? ","Rep1","Rep2","Rep3","Rep4","A"]);
+questionsCultureG.push(["question culture1 ","Rep1","Rep2","Rep3","Rep4","C"]);
+
 console.log(questionsInsolite);
+console.log(questionsInsolite[0][1]);
+
+function start(themeChoice) {
+  document.getElementById("question").innerHTML = themeChoice[0][0];
+  document.getElementById("reponse1").innerHTML = themeChoice[0][1];
+  document.getElementById("reponse2").innerHTML = themeChoice[0][2];
+  document.getElementById("reponse3").innerHTML = themeChoice[0][3];
+  document.getElementById("reponse4").innerHTML = themeChoice[0][4];
+}
+
+
+
+function insolite() {
+  themeChoice = questionsInsolite;
+  console.log(themeChoice);
+}
+
+function cultureG() {
+  themeChoice = questionsCultureG;
+  console.log(themeChoice);
+
+}
 
 function timer() {
   timeLeft = 15;
